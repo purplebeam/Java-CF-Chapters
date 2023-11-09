15e1349ef7178a5f7dfff173fd96519e65568362
purplebeam <konvpapageorgiou@gmail.com>
2023-11-09 16:53:03 +0200

    9/11/2023 Training and CFCourse

---
+++ src/gr/aueb/codingfactory/ch4/Stars10Vertical.java
@@ -0,0 +1,14 @@
+package gr.aueb.codingfactory.ch4;
+
+/**
+ * Εκτυπώνει 10 κάθετα stars.
+ */
+public class Stars10Vertical {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i<=10; i++){
+            System.out.println("*");
+        }
+    }
+}
