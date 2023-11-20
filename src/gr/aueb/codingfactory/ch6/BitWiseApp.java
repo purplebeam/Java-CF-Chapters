e936216ebd55b00d1f7d857f5885ef4561b6f19e
konstantinos_pap <konvpapageorgiou@gmail.com>
2023-11-20 18:25:47 +0200

    20/11/2023 Training and CFCourse

--- src/gr/aueb/codingfactory/ch6/BitWiseApp.java
+++ src/gr/aueb/codingfactory/ch6/BitWiseApp.java
@@ -6,10 +6,26 @@ public class BitWiseApp {
 
     }
 
+    /**
+     * Simulates the XOR operator by using
+     * only AND, OR, NOT logical operators.
+     *
+     * @param b1  the first boolean.
+     * @param b2  the second boolean value.
+     * @return the XOR result.
+     */
     public static boolean XOR(boolean b1, boolean b2){
         return (b1 && !b2) || (!b1 && b2);
     }
 
+
+    /**
+     * Returns the reverse digit (0/1) for
+     * every digit of the source array. Essentially,
+     * it returns the One's Compliment.
+     * @param binaryVector the source array.
+     * @return the compliment by  one of the source array
+     */
     public static int[] complimentByOne(int[] binaryVector){
         int[] binaryOut;
         if(binaryVector == null) return null;
